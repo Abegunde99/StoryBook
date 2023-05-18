dbaa5afb1c6a3c49888373a95f069b89fd8d7fad
Abegunde99 <preciousolanrewaju1998@gmail.com>
2023-05-18 15:39:00 +0100

    I fixed the mongo-connect error as well as the req.user errors

--- app.js
+++ app.js
@@ -1,63 +1,103 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const { engine } = require('express-handlebars');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const morgan = require('morgan');
-const indexRouter = require('./routes/index');
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const connectDB = require('./config/db')
 
-//load config
-dotenv.config({ path: './config/config.env' });
+// Load config
+dotenv.config({ path: './config/config.env' })
 
-//connect to database
-const connectDB = require('./config/db');
-connectDB();
+// Passport config
+require('./config/passport')(passport)
 
-//passport config
-require('./config/passport')(passport);
+connectDB()
 
-const app = express();
+const app = express()
 
-//handlebars
-app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.set('views', 'views');
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-//static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Method override
+// app.use(
+//   methodOverride(function (req, res) {
+//     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//       // look in urlencoded POST bodies and delete it
+//       let method = req.body._method
+//       delete req.body._method
+//       return method
+//     }
+//   })
+// )
 
-//body parser
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-//session middleware
-app.use(session({
+// Handlebars Helpers
+// const {
+//   formatDate,
+//   stripTags,
+//   truncate,
+//   editIcon,
+//   select,
+// } = require('./helpers/hbs')
+
+// Handlebars
+app.engine(
+  '.hbs',
+  engine({
+    // helpers: {
+    //   formatDate,
+    //   stripTags,
+    //   truncate,
+    //   editIcon,
+    //   select,
+    // },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+)
+app.set('view engine', '.hbs')
+
+// Sessions
+app.use(
+  session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
-}))
+    store: MongoStore.create({mongoUrl: process.env.MONGOURI,}),
+  })
+)
 
-//passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+  next()
+})
 
-//logging
-if (process.env.NODE_ENV == 'development') {
-    app.use(morgan('dev'));
-}
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+// app.use('/stories', require('./routes/stories'))
 
-//routes
-app.use('/auth', require('./routes/auth'));
-app.use('/', indexRouter);
+const PORT = process.env.PORT || 3000
 
-const PORT = process.env.PORT || 5000;
-
-
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
